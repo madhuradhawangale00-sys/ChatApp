f002dc26e72374f26fcff0772e5ef46eca7691ae
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-08-22 00:16:55 +0530

    updating home page

---
+++ client/src/components/ChatContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatComponent = () => {
+  return (
+    <div>
+      <h1>Chat Container</h1>
+    </div>
+  )
+}
+
+export default ChatComponent
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import RightSidebar from '../components/RightSidebar'
+import ChatContainer from '../components/ChatContainer'
 
 const HomePage = () => {
+
+    const [selectedUser, setSelectedUser] = useState(false);
+
+
+
   return (
-    <div>
-      <h1 className="text-white">Home Page</h1>
-    </div>
+
+
+    
+      <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+        <div className={`backdrop-blur-xl border-2 border-gray-600 rounder-2xl 
+            overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'} `}> 
+            <Sidebar />
+            <ChatContainer />
+            <RightSidebar />
+        </div>
+      </div>
+    
   )
 }
 
