43cb98c1eb7b1234cb05e4bb75f97e6106c1544e
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-08-25 00:55:29 +0530

    setting rightside bar

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const RightSidebar = () => {
-  return (
-    <div>
-      <h1>Right Sidebar</h1>
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
+    <div className={`bg-[#818582]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? "max-md:hidden" : ""} `}>
+      <div>
+        <img src= {selectedUser?.profilePic || assets.avatar_icon} alt="Profile Picture"
+        className="w-16 h-16 rounded-full object-cover" />
+        <h1 className="text-xl font-medium mx-auto flex items-center gap-2">
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {selectedUser.fullName}</h1>
+          <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
     </div>
   )
 }
