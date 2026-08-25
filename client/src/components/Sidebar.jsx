0687e3dd344a9b63000c87c6bf02d27566577d58
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-08-26 01:33:47 +0530

    Fix login page display issue and assets import

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
               <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
               <hr className="my-2 border-t border-gray-500"/>
-              <p className='cursor-pointer text-sm'>Logout</p>
+              <p onClick={() => navigate('/login')} className='cursor-pointer text-sm'>Logout</p>
             </div>
 
           </div>
