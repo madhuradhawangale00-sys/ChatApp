5317618b5ccb7f11cd1d8bf26d4caea985f7b005
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-09-12 23:07:44 +0530

    created database

---
+++ server/container/userController.js
@@ -0,0 +1,44 @@
+import bcrypt from "bcrypt";
+import { generateToken } from "../lib/utils";
+import User from "../model/User.js";
+
+//signup a NEW user
+export const signup = async (req,res) => {
+    const { fullname, email, password, bio} = req.body;
+
+    try{
+        if(!fullname || !email || !password){
+            return res.json({success: false, message: "Missing Details"})
+    }
+    const user = await User.findOne({email});
+
+    if(user){
+        return res.json({success: false, message:"Account already exists"});
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+        fullName, email,password: hashedPassword, bio
+    });
+
+    const token = generateToken(newUser._id)
+
+    res.json({success: true, userData: newUser, token, message:"Account created successfully"})
+
+} catch(error){
+   res.json({success: false, message: error.message})
+}
+}
+
+//controller to login user
+const login = async( req, res) => {
+    try{
+
+        const { email, password } = req.body;
+        const userData = await User.findOne({email})
+
+    } catch(error){
+
+    }
+}
