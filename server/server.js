8b4c3f0b18e7eb8c1e2caf99e4aa352cc24302ac
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-09-12 22:34:44 +0530

    created database

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from  "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 //cretae express app and http server
 const app = express();
@@ -15,6 +16,9 @@ app.use(cors());
 app.use("/api/status", (req, res) => {
     res.send("Server is running");
 });
+
+//connect to mongodb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=>console.log(`Server is runnig at ${PORT}`));
