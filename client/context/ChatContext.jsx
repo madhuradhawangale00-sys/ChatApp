353c2c65e6cba7ad0031f06214e6ffbdef5e3abf
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-09-15 14:32:29 +0530

    creating functions for ChantContext

---
+++ client/context/ChatContext.jsx
@@ -0,0 +1,72 @@
+import { createContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import toast from "react-hot-toast";
+
+
+export const ChatContext = createContext();
+
+export const ChatProvider = ({children})=>{
+
+    const [messages, setMessages] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [unseenMessages, setUnseenMessages] = useState({})
+
+    const {socket, axios} = useContext(AuthContext);
+
+    // function to get all users for sidebar
+    const getUsers = async ()=> {
+        try {
+           const {data} = await axios.get("/api/messages/users");
+           if(data.success){
+            setUsers(data.users)
+            setUnseenMessages(data.unseenMessages)
+           }
+        } catch (error) {
+            toast.error(error.message)
+            
+        }
+    }
+
+    // function to get messages for selected user
+    const getMessages = async (userId)=>{
+        try {
+           const {data} =  await axios.get(`/api/messages/${userId}`);
+           if(data.success){
+            setMessages(data.messages)
+           }
+        } catch (error) {
+            toast.error(error.message)
+
+            
+        }
+    }
+
+    //function  to send message to selected user
+    const sendMessage = async (messageData)=>{
+          try {
+            const {data} = await axios.post(`/api/messages/send/${selectedUser._id}`, messageData);
+            if(data.success){
+                setMessages((prevMessages)=>[...prevMessages, data.newMessage])
+            }else{
+                toast.error(data.message);
+            }
+          } catch (error) {
+            toast.error(error.message);
+          }
+          
+    }
+
+    const value = {
+
+    }
+
+
+}
+
+return (
+    <ChatContext.Provider value={value}>
+        {children}
+    </ChatContext.Provider>
+)
+
